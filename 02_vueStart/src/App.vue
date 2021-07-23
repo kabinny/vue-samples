bd027f8c1a7acc738c82dd728b39cb43d5d5bf2a
kabinny <kabinny@gmail.com>
2021-07-24 00:13:14 +0900

    02 vue start

---
+++ 02_vueStart/src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <button @click="handler();reverseMsg()">Click me!</button>
+    <h1 v-if="isShow">
+      {{ msg }}
+    </h1>
+    <h2>{{ reversedMsg }}</h2>
+    <ul>
+      <li v-for="fruit in fruits" v-bind:key="fruit">
+        {{ fruit }}
+      </li>
+    </ul>
+    <ul>
+      <Fruit v-for="fruit in fruits" 
+             :key="fruit" 
+             :fruit-name="fruit" />
+    </ul>
+  </div>
+</template>
+
+<script>
+import Fruit from './components/Fruit.vue'
+
+export default {
+  components: {
+    Fruit
+  },
+  data () {
+    return {
+      msg: 'Hello Vue?!',
+      reversedMsg: '',
+      isShow: true,
+      fruits: [
+        'Apple', 'Banana', 'Cherry', 'Mango'
+      ]
+    }
+  },
+  beforeCreate() {
+    console.log('beforeCreate:', this.msg)
+  },
+  created() {
+    console.log('created:', this.msg)
+  },
+  beforeMount() {
+    console.log('beforeMount:', document.querySelector('button'))
+  },
+  mounted() {
+    console.log('mounted:', document.querySelector('button'))
+  },
+  methods: {
+    handler() {
+      this.isShow = !this.isShow
+    },
+    reverseMsg() {
+      this.reversedMsg = this.msg.split('').reverse().join('')
+    }
+  }
+}
+</script>
---
+++ 02_vueStart/src/components/Fruit.vue
@@ -0,0 +1,22 @@
+<template>
+  <li class="list-item">
+    {{ fruitName }}
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    fruitName: String
+  }
+}
+</script>
+
+<style scoped>
+  li {
+    color: salmon;
+  }
+  li::after {
+    content: '!';
+  }
+</style>>
