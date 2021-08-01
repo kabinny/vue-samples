26f7e07973807ce7f1819ef239d5d86e90b6db0b
kabinny <kabinny@gmail.com>
2021-08-01 21:59:07 +0900

    webpack 기본 세팅, vuex 사용 복습

---
+++ 07_webpack_vue/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <hello />
+    <router-view />
+  </div>
+</template>
+
+<script>
+import Hello from '@/components/Hello.vue'
+
+export default {
+  components: {
+    Hello
+  }
+}
+</script>
---
+++ 07_webpack_vue/src/components/Hello.vue
@@ -0,0 +1,40 @@
+<template>
+  <h1
+    id="hello"
+    class="hello"
+    title="hello"
+    @click="reverseMsg">
+    {{ msg }}
+  </h1>
+</template>
+
+<script>
+// import { mapState } from 'vuex'
+
+export default {
+  computed: { // 데이터를 가져올 때
+    msg() {
+      return this.$store.state.message.msg
+    },
+    reversedMsg() {
+      return this.$stor.getters['message/reversedMsg']
+    }
+  },
+  methods: { // 동작을 가져올 때
+    updateMsg() {
+      this.$store.commit('message/updateMsg', '1234')
+    },
+    reverseMsg() {
+      this.$store.dispatch('message/reverseMsg')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+h1 {
+  $color: salmon;
+  background-color: $color;
+  display: flex; 
+}
+</style>
